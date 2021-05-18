2d6a5bac85f6206fd10f6e6c8e075ef886936305
jinho <gogod23@ajou.ac.kr>
2021-05-18 16:44:05 +0900

    feat : history detail - host table 생성

--- src/components/history/HostHistoryDetail.jsx
+++ src/components/history/HostHistoryDetail.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Table } from "antd";
 
-const HostHistoryDetail = () => {
-  return <div></div>;
+const HostHistoryDetailWrapper = styled.div``;
+
+const TableWrapper = styled.div`
+  border: 1px solid red;
+`;
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    width: 150,
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    width: 150,
+    render: (text) => <span style={{ color: "#888" }}>{text}</span>,
+  },
+  {
+    title: "Attitude",
+    dataIndex: "attitude",
+    width: 150,
+    render: (text) => <span style={{ color: "#888" }}>{text}</span>,
+  },
+  {
+    title: "Absence Time",
+    dataIndex: "absenceTime",
+    width: 150,
+    render: (text) => <span style={{ color: "#888" }}>{text}</span>,
+  },
+  {
+    title: "Attendance",
+    dataIndex: "attendace",
+    width: 150,
+    render: (text) =>
+      text === "FAIL" ? (
+        <span style={{ color: "red" }}>{text}</span>
+      ) : (
+        <span style={{ color: "blue" }}>{text}</span>
+      ),
+  },
+];
+
+const HostHistoryDetail = ({ data }) => {
+  const [newData, setNewData] = useState(
+    data.groupMember.map((v, i) => ({
+      key: i,
+      name: v.name,
+      email: v.email,
+      attitude:
+        v.attitude === 1 ? "Bad" : v.attitude === 2 ? "Average" : "Good",
+      absenceTime: v.absenceTime + " minutes",
+      attendace: v.isAttend ? "PASS" : "FAIL",
+    })),
+  );
+
+  return (
+    <HostHistoryDetailWrapper>
+      <TableWrapper>
+        <Table
+          columns={columns}
+          dataSource={newData}
+          scroll={{ y: 450 }}
+          pagination={false}
+        />
+        ,
+      </TableWrapper>
+    </HostHistoryDetailWrapper>
+  );
 };
 
 export default HostHistoryDetail;
